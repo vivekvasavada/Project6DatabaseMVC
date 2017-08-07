77f5ee60137d176898d597e786ac11e5acf0bf34
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-08-06 20:32:36 -0400

    initial commit

---
+++ Wardrobe/Controllers/BottomsIDsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wardrobe.Models;
+
+namespace Wardrobe.Controllers
+{
+    public class BottomsIDsController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: BottomsIDs
+        public ActionResult Index()
+        {
+            return View(db.BottomsIDs.ToList());
+        }
+
+        // GET: BottomsIDs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BottomsID bottomsID = db.BottomsIDs.Find(id);
+            if (bottomsID == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottomsID);
+        }
+
+        // GET: BottomsIDs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BottomsIDs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BottomsID1,BottomsName,BottomsPhoto,BottomsType,BottomsColor,BottomsSeason,BottomsOccasion")] BottomsID bottomsID)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BottomsIDs.Add(bottomsID);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bottomsID);
+        }
+
+        // GET: BottomsIDs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BottomsID bottomsID = db.BottomsIDs.Find(id);
+            if (bottomsID == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottomsID);
+        }
+
+        // POST: BottomsIDs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BottomsID1,BottomsName,BottomsPhoto,BottomsType,BottomsColor,BottomsSeason,BottomsOccasion")] BottomsID bottomsID)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bottomsID).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bottomsID);
+        }
+
+        // GET: BottomsIDs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BottomsID bottomsID = db.BottomsIDs.Find(id);
+            if (bottomsID == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottomsID);
+        }
+
+        // POST: BottomsIDs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BottomsID bottomsID = db.BottomsIDs.Find(id);
+            db.BottomsIDs.Remove(bottomsID);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
